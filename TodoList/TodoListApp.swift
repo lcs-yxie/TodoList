6332b9d2a2be90a21abaed3ea0ea04ab704b0fe0
Yukun Xie <yxie@lcs.on.ca>
2024-04-05 09:03:38 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Yukun Xie on 2024/4/5.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
