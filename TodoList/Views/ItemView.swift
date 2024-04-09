5754536769783ab7dcadfae27f0b6a3080021e5a
Yukun Xie <yxie@lcs.on.ca>
2024-04-09 14:26:07 -0400

    Made to-do items editable and responsive to size of the text they contain.

--- TodoList/Views/ItemView.swift
+++ TodoList/Views/ItemView.swift
@@ -7,7 +7,7 @@ struct ItemView: View {
     var body: some View {
         Label (
             title:{
-                Text (currentItem.title)
+                TextField("Enter a to-do item", text: $currentItem.title, axis: .vertical)
             }, icon: {
                 Image(systemName: currentItem.done == true ? "checkmark.circle":
                         "circle")
